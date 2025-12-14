ce1c2cd65f39c2689508abc7a38f290dd791c6f9
Roger Pence <roger.pence@gmail.com>
2025-12-14 12:18:43 -0600

    chore: build for release v0.0.3

---
+++ dist/components/HelloWorld.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	interface Props {
+		message: string;
+	}
+
+	let { message = 'World, World' }: Props = $props();
+</script>
+
+<div>
+	{message}
+</div>
---
+++ dist/components/HelloWorld.svelte.d.ts
@@ -0,0 +1,6 @@
+interface Props {
+    message: string;
+}
+declare const HelloWorld: import("svelte").Component<Props, {}, "">;
+type HelloWorld = ReturnType<typeof HelloWorld>;
+export default HelloWorld;
